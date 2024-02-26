a11058af4de090d02a71ef8415869dbb87acea4e
noesterle <noesterle1@gmail.com>
2024-02-26 17:21:46 -0500

    Add ability to sort lyrics into arrays for table.

--- quiz/quiz.py
+++ quiz/quiz.py
@@ -23,10 +23,38 @@ def quiz():
         
         album = random.choice(list(songs_by_album['albums']))
         song = random.choice(songs_by_album['albums'][album])
-        
+
         song_lyrics_arr = lyrics(band, song)
         song_lyrics = song_lyrics_arr[0]
+        song_lyrics_lst = sanitize_lyrics(song_lyrics)
+        song_lyrics_lst = organize_lyrics(song_lyrics_lst)
     else:
         album = "N/A"
         song = "N/A"
-    return render_template("quiz.jinja", title="Fill In The Blank", band=band, album=album, song=song, lyrics=song_lyrics)
+    return render_template("quiz.jinja", title="Fill In The Blank", band=band, album=album, song=song, lyrics=song_lyrics, lyrics_lst=song_lyrics_lst)
+
+def sanitize_lyrics(lyrics):
+    # Clean data
+    lyrics = lyrics.replace("\r"," ").replace("\n"," ")
+    # lyrics = ''.join(filter(str.isalpha or str.whitespace, lyrics))
+    lst = lyrics.split(" ",)
+    lst = list(filter(lambda a: a != "", lst))
+    # TODO: Find a way to get rid of all punctuation. Except for apostrophes?.
+    # TODO: Clean up words like "Ooh" -> "Oh"
+
+    print(lst)
+    return lst
+
+def organize_lyrics(lyrics_arr):
+    lyrics_arr = lyrics_arr
+    col_len = 50
+    table = []
+    col = []
+
+    while lyrics_arr != []:
+        # TODO: Adjust indices, likely off by 1.
+        col = lyrics_arr[:50]
+        lyrics_arr = lyrics_arr[50:]
+        table.append(col)
+
+    return table
